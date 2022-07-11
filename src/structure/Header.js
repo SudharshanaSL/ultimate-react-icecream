0d14a5ee7ad536c84d9111625a5697890b896f74
Sudharshana S L <15.hari.hari@gmail.com>
2022-07-11 18:39:36 +0530

    Adding menu item

--- src/structure/Header.js
+++ src/structure/Header.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import iceCream from '../assets/img/ultimate-ice-cream.svg';
+/**
+ * Use NavLink component when we have to make routing accessible.
+ * Note that routes should already be registered.
+ * It sets aria-current attribute to page when we are on the target page
+ * Also we can indicate it to set an active class using prop activeClassName
+ */
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
@@ -7,6 +14,11 @@ const Header = () => (
       <img src={iceCream} alt="" />
       Ultimate Ice Cream
     </h1>
+    <nav aria-label="Main navigation">
+      <NavLink to="/" activeClassName="active" exact>
+        Menu
+      </NavLink>
+    </nav>
   </header>
 );
 
